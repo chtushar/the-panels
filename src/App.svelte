5264f89792b91268559d0c4f936471f970e13891
Tushar Choudhari <choudharit23@gmail.com>
2022-11-27 14:41:07 +0530

    :sparkles; Render resize handler

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,28 @@
 <script lang="ts">
-  import { writable } from 'svelte/store';
-  import { ROOT_PADDING } from './constants';
+  import { panelsWithDimensions, dimensions, PanelType, SplitOrientation } from './panels';
+  import clsx from 'clsx';
 
-  import Panel from "./lib/Panel.svelte";
-  import { panels, dimensions } from './panels';
-
+  panelsWithDimensions.subscribe((panels) => {
+    console.table(panels);
+  });
 </script>
 
 <main class="flex flex-col items-center justify-center h-full p-4 dark:bg-neutral-900 bg-slate-50">
-  <div class="w-full h-full max-w-[1200px] max-h-[600px] dark:bg-slate-800 bg-slate-100 border-2 border-solid dark:border-slate-700 border-slate-200 rounded-lg relative flex gap-4 p-4">
+  <div class="w-full h-full max-w-[1000px] max-h-[600px] dark:bg-slate-800 bg-slate-100 border-2 border-solid dark:border-slate-700 border-slate-200 rounded-lg relative flex gap-4 p-4">
     <div class="relative bg-transparent w-full h-full" bind:clientWidth={$dimensions.width} bind:clientHeight={$dimensions.height}>
-      <Panel {...$panels.layoutSettings} />
+      {#each Object.keys($panelsWithDimensions) as id}
+        {#if $panelsWithDimensions[id].type === PanelType.Split}
+          <div
+            class="absolute dark:bg-slate-900 bg-slate-500 rounded-md opacity-0"
+            style="left: {$panelsWithDimensions[id].x}px; top: {$panelsWithDimensions[id].y}px; width: {$panelsWithDimensions[id].width}px; height: {$panelsWithDimensions[id].height}px;"
+          />
+        {/if}
+        {#if $panelsWithDimensions[id].type === PanelType.Resize}
+          <div class="absolute dark:bg-neutral-900 opacity-10"
+            style="left: {$panelsWithDimensions[id].x}px; top: {$panelsWithDimensions[id].y}px; width: {$panelsWithDimensions[id].width}px; height: {$panelsWithDimensions[id].height}px;"
+          />
+        {/if}
+      {/each}
     </div>
   </div>
 </main>
--- src/panels.ts
+++ src/panels.ts
@@ -1,6 +1,5 @@
-import { onMount } from 'svelte';
-import { writable } from 'svelte/store';
-import { ROOT_PADDING } from './constants';
+import { nanoid } from 'nanoid';
+import { derived, writable } from 'svelte/store';
 
 
 export enum SplitOrientation {
@@ -11,6 +10,7 @@ export enum SplitOrientation {
 export enum PanelType {
     Root = 'root',
     Split = 'split',
+    Resize = 'resize',
 }
 
 export interface PanelInterface {
@@ -36,33 +36,125 @@ export const panels = writable<{
                 id: '3La5i2dZci',
                 type: PanelType.Split,
                 orientation: SplitOrientation.Horizontal,
-                percent: 33.33333333333333,
+                percent: 20,
             },
             {
                 id: 'PEXjXDeaq2',
                 type: PanelType.Split,
                 orientation: SplitOrientation.Horizontal,
-                percent: 33.33333333333333,
+                percent: 30,
             },
             {
                 id: 'zQCYKjFLLk',
                 type: PanelType.Split,
-                orientation: SplitOrientation.Horizontal,
-                percent: 33.33333333333333,
+                orientation: SplitOrientation.Vertical,
+                percent: 50,
+                children: [
+                    {
+                        id: 'lU7nIxd7B9',
+                        type: PanelType.Split,
+                        orientation: SplitOrientation.Horizontal,
+                        percent: 50,
+                        children: [
+                            {
+                                id: 'oSC_U2Vcyn',
+                                type: PanelType.Split,
+                                orientation: SplitOrientation.Horizontal,
+                                percent: 50,
+                            },
+                            {
+                                id: 'WtaDeMFoUm',
+                                type: PanelType.Split,
+                                orientation: SplitOrientation.Horizontal,
+                                percent: 50,
+                            }
+                        ],
+                    },
+                    {
+                        id: '_BkpAPvUb-',
+                        type: PanelType.Split,
+                        orientation: SplitOrientation.Horizontal,
+                        percent: 50,
+                    }
+                ]
             }
         ]
     }
 });
 
-export const panelDimensions = (node, params) => {
-    const { id, type, orientation, percent } = params;
-    const isRoot = type === PanelType.Root;
-    const isSplit = type === PanelType.Split;
-    const isHorizontal = orientation === SplitOrientation.Horizontal;
-    const isVertical = orientation === SplitOrientation.Vertical;
+export const panelsWithDimensions = derived([dimensions, panels], (value) => {
+    const [dimensions, panels] = value;
+    const { layoutSettings } = panels;
+    let allPanels = {};
 
-    dimensions.subscribe(dimensions => {
-        node.style.width = `${dimensions.width * (percent / 100)}px`;
-        node.style.height = `${dimensions.height}px`;
-    });
-}
+    const addToAllPanels = (id: string, dimensions) => {
+        allPanels[id] = dimensions;
+    }
+
+    const getPanelDimensions = (panel: PanelInterface & { index: number }, parentDim) => {
+        if (panel.type === PanelType.Root) {
+            return {
+                ...parentDim,
+                index: panel.index,
+                x: 0,
+                y: 0,
+                resizeHandler: false
+            };
+        }
+    
+        const { width, height, orientation, maxIndex } = parentDim;
+        const { percent } = panel;
+    
+        const isHorizontal = orientation === SplitOrientation.Horizontal;
+        const isVertical = orientation === SplitOrientation.Vertical;
+        const resizeHandler = panel.type === PanelType.Split && maxIndex !== panel.index;
+    
+        const dim = {
+            width:  isHorizontal ? width * percent / 100 - (resizeHandler ? 8 : 0) : width,
+            height: isVertical ? height * percent / 100 - (resizeHandler ? 8 : 0): height,
+            orientation: panel.orientation,
+            parentOrientation: orientation,
+            type: panel.type,
+            index: panel.index,
+            x: 0,
+            y: 0,
+            resizeHandler,
+        };
+    
+        return dim;
+    }    
+    
+    const resizeHandle = (panelDim) => {
+            const x = panelDim.parentOrientation === SplitOrientation.Horizontal ? panelDim.x + panelDim.width : panelDim.x;
+            const y = panelDim.parentOrientation === SplitOrientation.Vertical ? panelDim.y + panelDim.height : panelDim.y;
+            const handle = {
+                id: nanoid(10),
+                type: PanelType.Resize,
+                orientation: panelDim.parentOrientation,
+                width: panelDim.parentOrientation === SplitOrientation.Horizontal ? 8 : panelDim.width,
+                height: panelDim.parentOrientation === SplitOrientation.Vertical ? 8 : panelDim.height,
+                x, y
+            }
+        addToAllPanels(handle.id, handle)
+    }
+
+    const traversePanel = (
+        panel: PanelInterface & { index: number }, 
+        parent: { width: number; height: number, orientation: SplitOrientation, type: PanelType }) => 
+    {
+            const dim = getPanelDimensions(panel, parent);
+            if (dim.resizeHandler) {
+                resizeHandle(dim);
+            }
+            
+            addToAllPanels(panel.id, dim);
+
+            if (panel.children) {
+                panel.children.forEach((p, i) => traversePanel({ ...p, index: i }, { ...dim, orientation: panel.orientation, maxIndex: panel.children.length - 1 }));
+            }
+    }
+
+    traversePanel({ ...layoutSettings, index: 0 }, { ...dimensions, orientation: layoutSettings.orientation, type: layoutSettings.type });
+
+    return allPanels;
+})
